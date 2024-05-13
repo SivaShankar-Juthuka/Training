e2b2ff3e365058c02e39bdfc7b5f8ec612c06e8a
Siva_Shankar <102404001+SivaShankar-Juthuka@users.noreply.github.com>
2024-05-13 09:20:12 +0530

    File Uploaded

---
+++ Assignment_2/que_12.rb
@@ -0,0 +1,69 @@
+class FolderStructureManager
+    def initialize
+        @continue = true
+    end
+
+    def create_folder_structure
+        while @continue
+            print "Enter Folder Name: "
+            folder_name = gets.chomp
+            print "Enter File Name: "
+            file_name = gets.chomp
+            print "What do you want to write in the file? - "
+            file_content = gets.chomp
+            # Create folder if it doesn't exist
+            Dir.mkdir(folder_name) unless Dir.exist?(folder_name)
+            # Write content to the file
+            File.open("#{folder_name}/#{file_name}", 'w') { |file| file.write(file_content) }
+            manage_files(folder_name)
+        end
+    end
+
+    private
+    def manage_files(folder_name)
+        print "\n Do you want to continue (Y/N)?: "
+        response = gets.chomp.downcase
+        if response == 'y'
+            print "\nDo you want to create/update/delete a given file?"
+            action = gets.chomp.downcase
+            if action == 'create'
+                create_file(folder_name)
+            elsif action == 'update'
+                update_file(folder_name)
+            else action == 'delete'
+                delete_file(folder_name)
+            end
+        else
+            @continue = false
+        end
+    end
+
+    def create_file(folder_name)
+        print "Enter File Name: "
+        file_name = gets.chomp
+        print "What do you want to write in the file? - "
+        file_content = gets.chomp
+        File.open("#{folder_name}/#{file_name}", 'w') { |file| file.write(file_content) }
+        manage_files(folder_name)
+    end
+
+    def update_file(folder_name)
+        print "Enter File Name: "
+        file_name = gets.chomp
+        print "What do you want to write in the file? - "
+        file_content = gets.chomp
+        File.open("#{folder_name}/#{file_name}", 'w') { |file| file.write(file_content) }
+        manage_files(folder_name)
+    end
+
+    def delete_file(folder_name)
+        print "Enter File Name: "
+        file_name = gets.chomp
+        File.delete("#{folder_name}/#{file_name}") if File.exist?("#{folder_name}/#{file_name}")
+        manage_files(folder_name)
+    end
+end
+  
+manager = FolderStructureManager.new
+manager.create_folder_structure
+  
